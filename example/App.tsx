bce3ff86f3ebf506da498b876de4500173ed438e
Stanislav Poleshuk <s.poleshuk@copybet.com>
2021-06-25 19:05:05 +0300

    onboarding

--- example/App.tsx
+++ example/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Dimensions, Image, ScrollView } from 'react-native';
+import Onboarding from '@stanislavpoleshuk/react-native-onboarding';
 
 const App = () => {
-  // return (
-  //   <Onboarding
-  //     data={[
-  //       {
-  //         slides: [{ source: require('./assets/Onbording_4.webp') }],
-  //       },
-  //     ]}
-  //   />
-  // );
+  return (
+    <Onboarding
+      data={[
+        {
+          slides: [{ source: require('./assets/Onbording_4.webp') }],
+        },
+      ]}
+    />
+  );
   return (
     <ScrollView contentInsetAdjustmentBehavior="automatic">
       <Image
--- src/components/collection/index.tsx
+++ src/components/collection/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { ICollection } from '../../interfaces';
 import { View } from 'react-native';
+import Slide from '../slide';
 
 type Props = ICollection & {};
 
-const Collection: React.FC<Props> = () => {
-  return <View />;
+const Collection: React.FC<Props> = ({ slides }) => {
+  return (
+    <View>
+      <Slide source={slides[0].source} />
+    </View>
+  );
 };
 
 export default Collection;
