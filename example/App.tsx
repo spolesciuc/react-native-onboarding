c97721d5f2aa7ccc21c1b775385dbec6a668f3da
Stanislav Poleshuk <s.poleshuk@copybet.com>
2021-06-25 22:48:01 +0300

    create package

--- example/App.tsx
+++ example/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Dimensions, Image, ScrollView } from 'react-native';
 import Onboarding from '@stanislavpoleshuk/react-native-onboarding';
 
 const App = () => {
@@ -11,17 +10,6 @@ const App = () => {
         },
       ]}
     />
-  );
-  return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <Image
-        source={require('./assets/Onbording_4.webp')}
-        style={{
-          height: Dimensions.get('window').height,
-          width: Dimensions.get('window').width,
-        }}
-      />
-    </ScrollView>
   );
 };
 
